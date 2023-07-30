43169347c4e241ea206c08afd4f9150345be1221
NFM <daniel.olovianniy@gmail.com>
2023-07-30 15:29:40 +0300

    added shop item activity

---
+++ app/src/main/java/ru/nfm/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,13 @@
+package ru.nfm.shoppinglist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.nfm.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
